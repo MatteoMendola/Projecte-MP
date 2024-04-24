286a784b3f6be9f52dfda60cd7d00a665a019d1c
MatteoMendola <matteotripleeme@gmail.com>
2024-04-24 18:13:54 +0200

    Funció inserir figura acabada

--- Projecte_part1/Figura.h
+++ Projecte_part1/Figura.h
@@ -54,6 +54,7 @@ public:
     void setFormaAct(int formaAct) { m_formaActual = formaAct; }
 
     void inicialitzarMatriuAuxiliar();  //s'inicialitza la matriu auxiliar sense girs
+    int nombreCaselles(TipusFigura tipus) const;    //nombre màxim de caselles en horitzontal o vertical que pot arribar a ocupar una figura (lletra I = 4, O = 2, la resta = 3)
     void moureLateralment(int dirX); //+1 esq, -1 dreta
     void baixarFigura() { m_posicioActual[0]++; }
     void girarFigura(DireccioGir dir);
@@ -63,7 +64,6 @@ private:
     int m_posicioActual[2]; //array que guarda les coordenades del centre de la figura dins del tauler, primera posició: fila, segona posició: columna
     int m_formaActual;  //0 – 3, segons ordre dels girs en sentit horari a la descripció de les figures (pàgina 8 del pwp de la 1a part)
     bool m_formaActualEnMatriu[MAX_ALCADA][MAX_AMPLADA];    //matriu que guarda la forma de la figura, true = hi ha peça, false = no hi ha
-    int nombreCaselles(TipusFigura tipus) const;    //nombre màxim de caselles en horitzontal o vertical que pot arribar a ocupar una figura (lletra I = 4, O = 2, la resta = 3)
     void transposarMatriu(int nCasMax);
     void invertirColumnes(int nCasMax);
     void invertirFiles(int nCasMax);
--- Projecte_part1/Tauler.cpp
+++ Projecte_part1/Tauler.cpp
@@ -2,5 +2,52 @@
 
 void Tauler::inserirFigura(const Figura& f)
 {
+	int nCasMax = f.nombreCaselles(f.getTipus());
+	bool matriuAux[MAX_ALCADA][MAX_AMPLADA];
+	f.getMatriuFormaAct(matriuAux);
+	int fila = f.getPosActFil();
+	int columna = f.getPosActCol();
+	ColorFigura color = f.getColor();
 
+	switch (nCasMax)
+	{
+	case 2:
+		break;
+	case 3:
+		fila -= 1;
+		columna -= 1;
+		break;
+	case 4:
+		if (f.getFormaAct() == 0)
+		{
+			fila -= 1;
+			columna -= 2;
+		}
+		else
+			if (f.getFormaAct() == 1)
+			{
+				fila -= 2;
+				columna -= 2;
+			}
+			else
+				if (f.getFormaAct() == 2)
+				{
+					fila -= 2;
+					columna -= 1;
+				}
+				else
+				{
+					fila -= 1;
+					columna -= 1;
+				}
+	}
+
+	for (int i = 0; i < nCasMax; i++)
+	{
+		for (int j = 0; j < nCasMax; j++)
+		{
+			if (matriuAux[i][j])
+				m_tauler[fila + i][columna + j] = color;
+		}
+	}
 }
